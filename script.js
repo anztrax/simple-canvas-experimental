7adb8153de79cdaaa44ba23f5061c624f51f312a
andrew ananta <kuro.ketanitem@gmail.com>
2016-07-07 22:04:59 +0700

    add greeceFlag & guyanaFlag

--- script.js
+++ script.js
@@ -14,12 +14,82 @@ $(function(){
   function doCanvas(){
     console.log('this browser support canvas');
     createPalauFlag(createCanvas(400,200));
+    createGreeceFlag(createCanvas(400,200));
+    createGuyanaFlag(createCanvas(400,200));
+    createGuyanaFlag(createCanvas(200,100));
   }
 
   function doCanvasNotSupported(){
     alert('your element is not supported canvas rendering');
   }
 });
+
+function createGuyanaFlag(canvasElem){
+  var canvas = canvasElem;
+  var context = canvas.getContext('2d');
+  var width = canvas.width;
+  var height = canvas.height;
+  var gap = (width /10) / 2.5;
+
+  context.fillStyle = '#009E49';
+  context.fillRect(0,0,width,height);
+  fillTriangle(context,0,0, width,height/2, 0,height,'white','white');
+  fillTriangle(context,0,gap, width - gap*2.5,height/2, 0,height-gap,'#FCD116','#FCD116');
+  fillTriangle(context,0,0, width/2,height/2, 0,height,'black','black');
+  fillTriangle(context,0,gap, width /2 - (gap*1.5),height/2, 0,height-gap,'#CE1126','#CE1126');
+}
+
+function fillTriangle(context, x1, y1, x2, y2, x3, y3, fillColor, strokeColor){
+  context.fillStyle = fillColor;
+  context.strokeStyle = strokeColor;
+  context.beginPath();
+  context.moveTo(x1,y1);
+  context.lineTo(x2,y2);
+  context.lineTo(x3,y3);
+  context.lineTo(x1, y1);
+  context.closePath();
+  context.stroke();
+  context.fill();
+}
+
+
+function createGreeceFlag(canvasElem){
+  var canvas = canvasElem;
+  var context = canvas.getContext('2d');    //we want to manipulate in 2d
+  var width = canvas.width;
+  var height = canvas.height;
+  var lineCount = 9;
+  var lineHeight = height / lineCount;
+  var offset = lineHeight / 2;
+
+  context.fillStyle = '#000080';
+  context.fillRect(0,0,width,height);
+
+  context.strokeStyle = '#ffffff';
+  context.lineWidth = lineHeight;
+
+  for(var i=0;i < lineCount;i++){
+    if(i % 2 == 1) {
+      context.moveTo(0, i * lineHeight + offset);
+      context.lineTo(width, i * lineHeight + offset);
+    }
+  }
+  //this stroke is not done, but if you group it it will be done by then
+  context.stroke();
+
+  //this is clear and start new drawing context
+  context.beginPath();
+  var rw = lineHeight * 5;
+  context.fillRect(0,0,rw,rw);
+
+  //stroke style in is white
+  context.moveTo(0,rw/2);
+  context.lineTo(rw,rw/2);
+
+  context.moveTo(rw/2,0);
+  context.lineTo(rw/2,rw);
+  context.stroke();
+}
 
 //mark : create canvas section
 function createPalauFlag(canvasElem){
